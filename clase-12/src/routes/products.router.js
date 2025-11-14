1c64b3cd359d6381b10d01161578f8e3a9f68a1b
Jean Paul Ferreira <jeanpaul@jepafe.net>
2025-11-13 21:54:05 -0300

    Clase 12 - Rutas, controladores y Servicios

---
+++ clase-12/src/routes/products.router.js
@@ -0,0 +1,15 @@
+import { Router } from "express";
+
+const router = Router();
+
+import {
+  getProducts,
+  getProductById,
+  createProduct,
+} from "../controllers/products.controller.js";
+
+router.get("/products", getProducts);
+router.get("/products/:id", getProductById);
+router.post("/products", createProduct);
+
+export default router;
